08c33e6ca203f837ad846ddb4d68fc750a518257
mhmd <mohammad.hammoud.lb@hotmail.com>
2021-07-08 07:23:51 +0300

    Trying new bin

--- bin/generate.js
+++ bin/generate.js
@@ -5,8 +5,8 @@ const { spawnSync } = require('child_process');
 const url = 'https://github.com/Mhmdhammoud/react-boiler-plate.git';
 
 spawnSync('git', ['clone', url, `${process.cwd()}/${appName}`]);
+spawnSync('cd', [`${appName}`]);
 spawnSync('npm', ['install']);
 
 console.log('Your app has been successfully installed');
-console.log(`$ cd ${appName}`);
 console.log('$ npm start');
